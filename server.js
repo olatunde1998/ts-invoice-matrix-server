e10f8a17dcb0ec57da1ccb3c1e51ca2b655567b0
geodevcode <geodevinstitute@gmail.com>
2024-08-09 11:39:20 +0100

    port 3001 added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const FRONTEND = process.env.FRONTEND;
 
 var corsOptions = {
