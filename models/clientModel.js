2c96c02aa85b7935b16d3c641e2f0b17be5bfe5a
geodevcode <geodevinstitute@gmail.com>
2024-08-10 16:56:36 +0100

    Client Model created

---
+++ models/clientModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const clientSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please enter First name"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please enter Last name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter Password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Client = mongoose.model("client", clientSchema);
+module.exports = Client;
